53ae7e94cfacc811da3ea98397900c6f3c646181
Puru-Rana <pururana24@gmail.com>
2023-10-02 21:30:10 -0400

    Done with Page Title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import PageTitle from "./components/PageTitle";
 
 function App() {
   return (
     <div>
-      <h1> Hello World</h1>
+      <PageTitle>To Do List</PageTitle>
     </div>
   );
 }
 
-export default App;
+export default App; 
